c3b4e114b481914b934b18a577b279c868dc73c9
Russ Lascala <russlascala@gmail.com>
2018-09-25 16:34:27 -0400

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,56 @@
+var currentTab = 0; // Current tab is set to be the first tab (0)
+showTab(currentTab); // Display the current tab
+
+function showTab(n) {
+    // This function will display the specified tab of the form ...
+    var x = document.getElementsByClassName("tab");
+    x[n].style.display = "block";
+    // bounces in the text for next 
+    x[n].classList.add('animated', 'bounceIn');  
+    
+
+    // ... and run a function that displays the correct step indicator:
+    fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+    // This function will figure out which tab to display
+    var x = document.getElementsByClassName("tab");
+    
+    // If the dropdown is selected with a value status is true, mark the step as finished:
+    if (currentTab.value == "") {
+        console.log('You need to make a selection');
+    } else {
+        document.getElementsByClassName("step")[currentTab].className += " finish";
+    }
+    // Hide the current tab:
+    // fades in the text for prev 
+    x[currentTab].classList.add('animated','fadeIn');
+    x[currentTab].style.display = "none";
+    // Increase or decrease the current tab by 1:
+    currentTab = currentTab + n;
+    // if you have reached the end of the form... :
+    if (currentTab >= x.length) {
+        //...the form gets submitted:
+        document.getElementById("regForm").submit();
+        return false;
+    }
+    // Otherwise, display the correct tab:
+    showTab(currentTab);
+    
+    
+}
+
+/* $('#phone-number-field').keyup(function () {
+    $(this).val($(this).val().replace(/(\d{3})\-?(\d{3})\-?(\d{4})/, '$1-$2-$3'))
+}); */
+
+function fixStepIndicator(n) {
+    // This function removes the "active" class of all steps...
+    var i, x = document.getElementsByClassName("step");
+    for (i = 0; i < x.length; i++) {
+        x[i].className = x[i].className.replace(" active", "");
+    }
+    //... and adds the "active" class to the current step:
+    x[n].className += " active";
+}
